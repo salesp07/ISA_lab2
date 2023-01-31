d050cafd9c6b95138c638e5af42d00da608fc2f8
Pedro Sales Muniz <46171624+salesp07@users.noreply.github.com>
2023-01-30 19:18:42 -0800

    changes undone

--- public/client.js
+++ public/client.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 		const message = $('#chatbot-input').val();
 		$.ajax({
 			type: 'POST',
-			url: 'http://localhost:3000/chatbot',
+			url: '/chatbot',
 			data: {
 				message: message
 			},
